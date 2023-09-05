7889766ad13d5f9108532af6a688bb7e00c21cd1
Francisco Ceballos <ceballosfranciscomartin@gmail.com>
2023-09-05 15:20:32 -0300

    Feat. Initial CleanUp
    - Added favicons
    - Installed Framer Motion

---
+++ components/Head/Head.js
@@ -0,0 +1,37 @@
+import Head from "next/head";
+
+function HeadComponent(props) {
+  const { title, description } = props;
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta
+          name="format-detection"
+          content="telephone=no, date=no, email=no, address=no"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/assets/img/favicons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/assets/img/favicons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/assets/img/favicons/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicons/site.webmanifest" />
+      </Head>
+    </>
+  );
+}
+
+export default HeadComponent;
