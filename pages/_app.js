443275df7e6b0460085c2aef930a8a2b6724b9d4
Francisco Ceballos <104146989+FranCeballos@users.noreply.github.com>
2023-09-27 17:39:05 -0300

    Feat. Added Vercel Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import { store } from "@/store/index";
 import "@/styles/globals.css";
 import { Provider } from "react-redux";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
   return (
     <Provider store={store}>
       <Component {...pageProps} />
+      <Analytics />
     </Provider>
   );
 }
